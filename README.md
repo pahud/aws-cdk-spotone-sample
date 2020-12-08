f0688ae0417e82b4c77e5354224cfe12fe944867
Pahud <pahudnet@gmail.com>
2020-12-08 21:57:44 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# my project
+[![awscdk-run](https://img.shields.io/badge/Deploy%20with-AWSCDK.RUN-blue)](https://awscdk.run)
+# aws-cdk-spotone-sample
+
+A sample CDK application to create one durable spot instance with [cdk-spot-one](https://github.com/pahud/cdk-spot-one)
+
+# sample
+
+```sh
+npx cdk deploy \
+-c use_default_vpc=1 \
+-c volume_size=100 \
+-c instance_type=m5.large \
+-c eip_allocation_id=eipalloc-0133b41b7fdff3018
+```
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,55 @@
-import { App, Construct, Stack, StackProps } from '@aws-cdk/core';
+import * as cdk from '@aws-cdk/core';
+import * as spotone from 'cdk-spot-one';
+import * as ec2 from '@aws-cdk/aws-ec2';
 
-export class MyStack extends Stack {
-  constructor(scope: Construct, id: string, props: StackProps = {}) {
-    super(scope, id, props);
 
-    // define resources here...
+export class Demo extends cdk.Construct {
+  constructor(scope: cdk.Construct, id: string) {
+    super(scope, id)
+    const instanceType = this.node.tryGetContext('instance_type') || 't3.large';
+    const eipAllocationId = this.node.tryGetContext('eip_allocation_id');
+    const volumeSize = this.node.tryGetContext('volume_size') || 60;
+
+    const vpc = spotone.VpcProvider.getOrCreate(this);
+
+    const fleet = new spotone.SpotFleet(this, 'SpotFleet', {
+      vpc,
+      blockDuration: spotone.BlockDuration.SIX_HOURS,
+      eipAllocationId: eipAllocationId,
+      defaultInstanceType: new ec2.InstanceType(instanceType),
+      blockDeviceMappings: [
+        {
+          deviceName: '/dev/xvda',
+          ebs: {
+            volumeSize,
+          },
+        },
+      ],
+    });
+
+    const expireAfter = this.node.tryGetContext('expire_after');
+    if (expireAfter) {
+      fleet.expireAfter(cdk.Duration.hours(expireAfter));
+    }
   }
 }
 
-// for development, use account/region from cdk cli
+export class MyStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props: cdk.StackProps = {}) {
+    super(scope, id, props);
+
+    new Demo(this, 'Demo');
+  }
+}
+
 const devEnv = {
   account: process.env.CDK_DEFAULT_ACCOUNT,
   region: process.env.CDK_DEFAULT_REGION,
 };
 
-const app = new App();
+const app = new cdk.App();
 
-new MyStack(app, 'my-stack-dev', { env: devEnv });
-// new MyStack(app, 'my-stack-prod', { env: prodEnv });
+const stackName = app.node.tryGetContext('stackName') || 'SpotOneFleet';
+new MyStack(app, stackName, { env: devEnv });
 
 app.synth();
